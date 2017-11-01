7100224cb82a9bbbc30d6e86ffa69d542e6659a6
deathofasellout <javascriptforlife@gmail.com>
2017-11-01 09:06:13 -0700

    working on devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_id(params[:id])
+    @users = User.all
   end
 
   def update
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,26 @@
 class User < ApplicationRecord
+  acts_as_messageable
+
   has_many :spots
   has_secure_password
+
+  has_many :friendships
+  has_many :friends, :through => :friendships
+
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
 
   validates :username, presence: true, uniqueness: true
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true, length: { in: 6..20 }
+
+  def name
+    "User #{id}"
+  end
+
+  def mailboxer_email(object)
+    nil
+  end
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
