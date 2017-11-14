415f91df4ccbba8a1e65c3484036abc3f95cacb3
deathofasellout <javascriptforlife@gmail.com>
2017-11-13 22:32:54 -0800

    cleanup

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,8 +1,6 @@
 class SpotsController < ApplicationController
   before_action :set_spot, only: [:show, :edit, :update, :destroy]
 
-  # GET /spots
-  # GET /spots.json
   def index
     @spot = Spot.new
     @spots = Spot.all
@@ -13,22 +11,16 @@ class SpotsController < ApplicationController
     end
   end
 
-  # GET /spots/1
-  # GET /spots/1.json
   def show
   end
 
-  # GET /spots/new
   def new
     @spot = Spot.new
   end
 
-  # GET /spots/1/edit
   def edit
   end
 
-  # POST /spots
-  # POST /spots.json
   def create
     @spot = Spot.new(spot_params)
 
@@ -43,14 +35,13 @@ class SpotsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /spots/1
-  # PATCH/PUT /spots/1.json
   def update
     @spot = Spot.new(spot_params)
 
     respond_to do |format|
       if @spot.update(spot_params)
-        format.html{ redirect_to @spot}
+        #redirect_to @spot
+        format.html{ redirect_to spots_url}
         format.json{ render :show, status: :ok, location: @spot}
       else
         format.html{ render :new}
@@ -59,8 +50,6 @@ class SpotsController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @spot.destroy
     respond_to do |format|
@@ -70,12 +59,10 @@ class SpotsController < ApplicationController
   end
 
   private
-   # Use callbacks to share common setup or constraints between actions.
    def set_spot
      @spot = Spot.find(params[:id])
    end
 
-   # Never trust parameters from the scary internet, only allow the white list through.
    def spot_params
      params.require(:spot).permit(:latitude, :longitude, :address, :description, :name, :features, :comments)
    end
