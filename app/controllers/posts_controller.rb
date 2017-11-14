74ab2c6b765ada399e15de24688aa90fe7c0d9f1
deathofasellout <javascriptforlife@gmail.com>
2017-11-13 22:53:15 -0800

    cleanup

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,7 +22,7 @@ class PostsController < ApplicationController
     @post.user_id = current_user.id
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to posts_url}
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
