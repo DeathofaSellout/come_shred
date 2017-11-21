1a3795f673e940430cc6b133b21f7b1d4f234e1f
deathofasellout <javascriptforlife@gmail.com>
2017-11-20 18:16:42 -0800

    changed post_comments_edit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,8 +2,10 @@ class MessagesController < ApplicationController
   before_action :set_conversation
 
   def create
+    # fix
     receipt = current_user.reply_to_conversation(@conversation, params[:body])
     redirect_to conversation_path(receipt.conversation)
+    # redirect_to conversations_url
   end
   def destroy
     @conversation = current_user.mailbox.conversations.find(params[:id])
--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,28 +1,20 @@
 class PostCommentsController < ApplicationController
   before_action :set_post_comment, only: [:show, :edit, :update, :destroy]
 
-  # GET /post_comments
-  # GET /post_comments.json
   def index
     @post_comments = PostComment.all
   end
 
-  # GET /post_comments/1
-  # GET /post_comments/1.json
   def show
   end
 
-  # GET /post_comments/new
   def new
     @post_comment = PostComment.new
   end
 
-  # GET /post_comments/1/edit
   def edit
   end
 
-  # POST /post_comments
-  # POST /post_comments.json
   def create
     @post_comment = PostComment.new(post_comment_params)
     @post_comment.user = current_user
@@ -31,9 +23,6 @@ class PostCommentsController < ApplicationController
     redirect_to posts_path
   end
 
-
-  # PATCH/PUT /post_comments/1
-  # PATCH/PUT /post_comments/1.json
   def update
     respond_to do |format|
       if @post_comment.update(post_comment_params)
@@ -46,20 +35,16 @@ class PostCommentsController < ApplicationController
     end
   end
 
-  # DELETE /post_comments/1
-  # DELETE /post_comments/1.json
   def destroy
     @post_comment.destroy
     redirect_to posts_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_post_comment
       @post_comment = PostComment.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def post_comment_params
       params.require(:post_comment).permit(:comment, :post_id, :user_id)
     end
